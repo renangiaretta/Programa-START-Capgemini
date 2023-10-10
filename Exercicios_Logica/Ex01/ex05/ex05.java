8863e0b3863044db727a1ac91ba301f97fcf1da9
renangiaretta <renan.giaretta@hotmail.com>
2023-10-10 15:34:18 -0300

    Exercicio 5 finalizado

---
+++ Exercicios_Logica/Ex01/ex05/ex05.java
@@ -0,0 +1,39 @@
+package Exercicios_Logica.Ex01.ex05;
+
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args) {
+        Scanner lScanner = new Scanner(System.in);
+        String studentName = "";
+        float sumOfGrades = 0;
+        float media = 0;
+        DecimalFormat toDecimal = new DecimalFormat("#.##");
+        for (int i = 0; i <= 3; i++) {
+            while (true) {
+                try {
+                    if (i == 0) {
+                        System.out.print("Digite o nome do aluno: ");
+                        studentName = lScanner.nextLine();
+                    } else {
+                        System.out.print("Digite o valor da nota " + (i) + ": ");
+                        float grade = lScanner.nextFloat();
+                        sumOfGrades = sumOfGrades + grade;
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Insira um valor válido.");
+                    lScanner.nextLine();
+                }
+            }
+        }
+        media = (sumOfGrades / 3);
+        String formatedMedia = toDecimal.format(media);
+        System.out.println("A média final do aluno "
+                + studentName
+                + " é " + formatedMedia
+                + ".");
+    }
+}
