0456c152f06c27ddbfee45ea9a722cc014f23520
renangiaretta <renan.giaretta@hotmail.com>
2023-10-11 11:18:14 -0300

    Exercicio 26 finalizado

---
+++ Exercicios_Logica/Ex01/ex26/ex26.java
@@ -0,0 +1,27 @@
+package Exercicios_Logica.Ex01.ex26;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ex26 {
+    public static void main(String[] args) {
+        Scanner lScanner = new Scanner(System.in);
+        String[] numberWords = {"Um", "Dois", "Três", "Quatro", "Cinco"};
+        while (true) {
+            try {
+                System.out.print("Digite um número: ");
+                int insertedNumber = lScanner.nextInt();
+                if (insertedNumber < 1 || insertedNumber > 5) {
+                    throw new InputMismatchException("Número inválido.");
+                }
+                System.out.println(numberWords[(insertedNumber - 1)]);
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage() != null
+                        ? e.getMessage()
+                        : "Número inválido");
+                lScanner.nextLine();
+            }
+            break;
+        }
+    }
+}
